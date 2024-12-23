29ba9b6931f9cacd585fd4d425de1fb7c470b455
tongxiaojun <tongxiaojun@redoop.com>
2024-12-23 13:37:10 +0800

    AMBARI-26268: Remove default value for ambari-java-home in ambari-server.py to fix setup handling #3915
    Co-authored-by: tongxiaojun <tongxiaojun@gmail.com>

--- ambari-server/src/main/python/ambari-server.py
+++ ambari-server/src/main/python/ambari-server.py
@@ -764,7 +764,6 @@ def init_setup_parser_options(parser):
   other_group.add_option(
     "--ambari-java-home",
     dest="ambari_java_home",
-    default=None,
     help="Use specified java_home for ambari.  Must be valid on Ambari server hosts",
   )
   other_group.add_option(
